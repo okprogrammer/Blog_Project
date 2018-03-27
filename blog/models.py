65340740c32aadc3ad11ff824a57ebf3a0b81953
okprogrammmer <omk7912@gmail.com>
2018-03-28 01:54:35 +0530

    adding other html files

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,10 @@ from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null=True)
 
     def publish(self):
@@ -27,7 +27,7 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post',related_name='comments')
+    post = models.ForeignKey('blog.Post',related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
     create_date = models.DateTimeField(default=timezone.now())
