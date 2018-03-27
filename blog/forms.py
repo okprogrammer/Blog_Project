230eb8ff0d8e37993e0cbd4ac6fc85140e7bd9d4
okprogrammmer <omk7912@gmail.com>
2018-03-28 01:52:55 +0530

    changing basic_stuffs

--- blog/forms.py
+++ blog/forms.py
@@ -19,6 +19,6 @@ class CommentForm(forms.ModelForm):
         fields = ('author','text')
 
         widgets = {
-            'author':forms.TextInput(attrs={'class':textinputclass}),
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
         }
